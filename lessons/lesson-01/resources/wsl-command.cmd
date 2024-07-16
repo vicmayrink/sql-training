5a28dd0bac2e5505a438d82e799793d0e81c8fd1
Victor Mayrink <mayrink.vtm@gmail.com>
2024-07-15 21:15:48 -0300

    updates on docker installation

---
+++ lessons/lesson-01/resources/wsl-command.cmd
@@ -0,0 +1,6 @@
+C:\Users\victor> wsl
+Welcome to Ubuntu 22.04.3 LTS (...)
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+(...)
